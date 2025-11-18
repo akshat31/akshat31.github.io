a39145aedb71aff8560cf70f6e7badf9c0496e67
Akshat Soni <akshat.soni31@gmail.com>
2025-11-18 15:37:43 +0530

    Introduce blog section

---
+++ assets/top-challenges-website-designers-face-CnhmBmm1.js
@@ -0,0 +1,32 @@
+const e=`# Compiling client expectations into code
+
+
+The most crucial task being a website designer is transforming client requirements into code if you are working as a freelancer and taking cut to cut responsibilities of client requirements, then in real-time you have to think from both perspectives that is with respect to code and design
+
+
+# Maintaining responsiveness across devices
+
+
+Maintaining responsiveness is one of the most crucial tasks because more than 70% of internet traffic is from mobile or tablet so any web app or website its for most important to work fine also in mobile device
+
+
+# Cross browser compatibility
+
+
+In this competitive world, the competition among web browsers also is at peak, majority of techie people consider chrome( i guess due to developer tool) for their work, but in meantime, common people also go with different browsers like opera, firefox, edge, IE, etc so it's very important for a website to run stable in any browser with any version, to make this happen in the background a web designer has to do a lot of testing and invest a lot of time to make website stable across different browsers.
+
+
+# Side effects of using many libraries
+
+
+If we are using some framework like react angular then this point is not valid at all because in that case, we get node package manager, yarn, etc package management tool, but if we are creating a website from just HTML CSS and javascript with CDN and script tag link in web page then big challenges comes into the picture like getting errors for cross libraries compatibility, not getting proper dependency from one library to anotherSay if we are using some carousal library and jquery is dependency for that library then we would have to make sure that jquery script comes first than carousal script also we need a proper version of jquery to be there to run that carousal properly
+
+
+# Cache issue
+
+
+As I mentioned earlier here we are talking about just HTML CSS and javascript made website native way of creating website until and unless if we are using some task runner or build tool like gulp, grunt, webpack etc till then we are somehow safe, if we aren’t using them then cache becomes most irritating part whenever you change something in CSS it doesn’t reflect and you would think I didn’t apply proper CSS after a struggle you would get to know that it hasn’t CSS problem it's because of shit cacheCache plays a very major role in every browser but irrespective of that it also creates lots of problems while creating a website most of the time it takes an old copy of CSS and js and our changes couldn’t reflect and we think it's our issue
+
+
+# Conclusion:-
+A lot more challenges a web designer face but here I tried to showcase some of the major and most common problem web designers come’s across, at last, i just wanna say not only web designing if we talk about graphics designing, etc those also is not  piece of cake, behind every design lot more dedication and time a designer invest to make it successful.`;export{e as default};
